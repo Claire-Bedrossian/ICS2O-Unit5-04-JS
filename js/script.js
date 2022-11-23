ee2a757c02f874eebae735a74d3ba0287f66e80d
Claire Bedrossian <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-11-23 19:47:44 +0000

    started adding html

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Copyright (c) 202X Claire Bedrossian All rights reserved
+// Copyright (c) 2022 Claire Bedrossian All rights reserved
 //
 // Created by: Claire Bedrossian
-// Created on: XXX 202X
+// Created on: Nov 2022
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -10,8 +10,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-04-JS/sw.js", {
+    scope: "/ICS2O-Unit5-04-JS/",
   })
 }
 
